c51e8b9cf344df1cccd9a21c815731ec2addb4fa
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-09-25 14:54:35 +0300

    added: Teacher entity.

---
+++ Homework-track-API/Entities/Teacher.cs
@@ -0,0 +1,12 @@
+namespace Homework_track_API.Entities;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? ProfileImagePath { get; set; }
+    
+}
