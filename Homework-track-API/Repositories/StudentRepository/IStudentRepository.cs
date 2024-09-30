af38b8c90b5d0bd244ab630d387c0d48acd1c387
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-01 01:34:37 +0300

    added:StudentRepository

---
+++ Homework-track-API/Repositories/StudentRepository/IStudentRepository.cs
@@ -0,0 +1,12 @@
+using Homework_track_API.Entities;
+
+namespace Homework_track_API.Repositories.StudentRepository;
+
+public interface IStudentRepository
+{
+    Task<IEnumerable<Student>> GetAllStudentsAsync();
+    Task<Student> GetStudentByIdAsync(int id);
+    Task<bool> DeleteStudentByIdAsync(int id);
+    Task<Student> CreateStudentAsync(Student student);
+    Task<Student> UpdateStudentAsync(Student student);
+}
---
+++ Homework-track-API/Repositories/StudentRepository/StudentRepository.cs
@@ -0,0 +1,56 @@
+using Homework_track_API.Data;
+using Homework_track_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework_track_API.Repositories.StudentRepository;
+
+public class StudentRepository:IStudentRepository
+{
+    private readonly HomeworkTrackDbContext _context;
+    
+    public StudentRepository(HomeworkTrackDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+    {
+        return await _context.Students.ToListAsync();
+    }
+
+    public async Task<Student> GetStudentByIdAsync(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        return student;
+    }
+
+    public async Task<Student> CreateStudentAsync(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+        return student;
+    }
+
+    public async Task<bool> DeleteStudentByIdAsync(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+        
+        return false;
+    }
+
+    public async Task<Student> UpdateStudentAsync(Student student)
+    {
+        _context.Students.Update(student);
+        await _context.SaveChangesAsync();
+        return student;
+    }
+
+
+}
