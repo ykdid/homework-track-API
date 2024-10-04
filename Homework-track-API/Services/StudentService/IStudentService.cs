b20ab6124a258746c2c9f89942f3bf778f76a339
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-04 18:31:45 +0300

    added: StudentService

---
+++ Homework-track-API/Services/StudentService/IStudentService.cs
@@ -0,0 +1,12 @@
+using Homework_track_API.Entities;
+
+namespace Homework_track_API.Services.StudentService;
+
+public interface IStudentService
+{
+    Task<IEnumerable<Student>> GetAllStudents();
+    Task<Student> GetStudentById(int id);
+    Task<bool> DeleteStudentById(int id);
+    Task<Student> CreateStudent(Student student);
+    Task<Student> UpdateStudent(Student student);
+}
---
+++ Homework-track-API/Services/StudentService/StudentService.cs
@@ -0,0 +1,147 @@
+using Homework_track_API.Repositories.StudentRepository;
+using Homework_track_API.Entities;
+
+namespace Homework_track_API.Services.StudentService;
+
+public class StudentService:IStudentService
+{
+    private readonly StudentRepository _studentRepository;
+
+    public StudentService(StudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
+    public async Task<IEnumerable<Student>> GetAllStudents()
+    {
+        return await _studentRepository.GetAllStudentsAsync();
+    }
+
+    public async Task<Student> GetStudentById(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid student ID.");
+        }
+
+        var student =  await _studentRepository.GetStudentByIdAsync(id);
+
+        if (student == null)
+        {
+            throw new KeyNotFoundException($"Student with ID {id} not found.");
+        }
+
+        return student;
+    }
+
+    public async Task<bool> DeleteStudentById(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid student ID.");
+        }
+
+        var student = await _studentRepository.GetStudentByIdAsync(id);
+
+        if (student == null)
+        {
+            throw new KeyNotFoundException($"Student with ID {id} not found.");
+        }
+
+        await _studentRepository.DeleteStudentByIdAsync(id);
+        
+        return true;
+    }
+
+    public async Task<Student> CreateStudent(Student student)
+    {
+        if (student ==  null)
+        {
+            throw new ArgumentNullException(nameof(student));
+        }
+        
+        student.Name = student.Name.Trim();
+        student.Surname = student.Surname.Trim();
+        
+        if (string.IsNullOrEmpty(student.Name) && string.IsNullOrEmpty(student.Surname))
+        {
+            throw new ArgumentException("Student name and surname cannot be empty.");
+        }
+        
+        if (student.Name.Contains(" ") || student.Surname.Contains(" "))
+        {
+            throw new ArgumentException("Name and surname should not contain spaces.");
+        }
+        
+        if (string.IsNullOrEmpty(student.StudentNumber))
+        {
+            throw new ArgumentException("Student number cannot be empty.");
+        }
+
+        if (student.StudentNumber.Length != 9 && !student.StudentNumber.All(char.IsDigit))
+        {
+            throw new ArgumentException("Student number should be 9-digit numeric value.");
+        }
+
+        await _studentRepository.CreateStudentAsync(student);
+        return student;
+    }
+
+    public async Task<Student> UpdateStudent(Student student)
+    {
+        if (student == null)
+        {
+            throw new ArgumentNullException(nameof(student), "Updated student object cannot be null.");
+        }
+        
+        var existingStudent = await _studentRepository.GetStudentByIdAsync(student.Id);
+
+        if (existingStudent == null)
+        {
+            throw new KeyNotFoundException($"Student with ID {student.Id} not found.");
+        }
+        
+        if (!string.IsNullOrWhiteSpace(student.Name))
+        {
+            existingStudent.Name = student.Name.Trim();
+        }
+        
+        if (!string.IsNullOrWhiteSpace(student.Surname))
+        {
+            existingStudent.Surname = student.Surname.Trim();
+        }
+        
+        if (!string.IsNullOrWhiteSpace(student.Email))
+        {
+            existingStudent.Email = student.Email.Trim();
+        }
+        
+        if (!string.IsNullOrWhiteSpace(student.Password))
+        {
+            existingStudent.Password = student.Password; 
+        }
+
+        if (!string.IsNullOrWhiteSpace(student.StudentNumber))
+        {
+            if (student.StudentNumber.Length == 9 && student.StudentNumber.All(char.IsDigit))
+            {
+                existingStudent.StudentNumber = student.StudentNumber;
+            }
+            else
+            {
+                throw new ArgumentException("Student number should be a 9-digit numeric value.");
+            }
+        }
+        
+        if (!string.IsNullOrWhiteSpace(student.ProfileImagePath))
+        {
+            existingStudent.ProfileImagePath = student.ProfileImagePath;
+        }
+        
+        await _studentRepository.UpdateStudentAsync(existingStudent);
+
+        return existingStudent;
+    }
+    
+    
+}
