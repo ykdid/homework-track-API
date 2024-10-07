59e06e16af3fadca00ed53abfdf090e8b712ed78
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-08 01:57:07 +0300

    added : initial HomeworkController

--- Homework-track-API/Controllers/HomeworkController.cs
+++ Homework-track-API/Controllers/HomeworkController.cs
@@ -1,6 +1,15 @@
-namespace Homework_track_API.Controllers;
+using Homework_track_API.Entities;
+using Homework_track_API.Services.HomeworkService;
+using Microsoft.AspNetCore.Mvc;
 
-public class HomeworkController
-{
-    
+namespace Homework_track_API.Controllers{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeworkController(IHomeworkService homeworkService):ControllerBase
+    {
+        private readonly IHomeworkService _homeworkService = homeworkService;
+        
+        
+    }
 }
