d41dbad3861b2fe3a97d8f008724c7d054984260
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-19 00:37:54 +0300

    added: migrations

---
+++ Homework-track-API/Migrations/20241018213440_EditedMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homework_track_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mark",
+                schema: "Operations",
+                table: "Submission",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                schema: "Courses",
+                table: "Course",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mark",
+                schema: "Operations",
+                table: "Submission");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "Courses",
+                table: "Course");
+        }
+    }
+}
