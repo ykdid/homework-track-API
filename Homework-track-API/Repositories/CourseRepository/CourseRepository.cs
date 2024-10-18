e0293808f3e8d4a1020dd941ebd73fe42ba93131
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-19 00:40:37 +0300

    added: course repo

---
+++ Homework-track-API/Repositories/CourseRepository/CourseRepository.cs
@@ -0,0 +1,79 @@
+using Homework_track_API.Data;
+using Homework_track_API.Entities;
+using Homework_track_API.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework_track_API.Repositories.CourseRepository;
+
+public class CourseRepository:ICourseRepository
+{
+    private readonly HomeworkTrackDbContext _context;
+
+    public CourseRepository(HomeworkTrackDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Course>> GetAllCoursesAsync()
+    {
+        return await _context.Courses
+            .Where(c => c.Status == CourseStatus.Active)
+            .ToListAsync();
+    }
+    
+    public async Task<Course> GetCourseByIdAsync(int id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+        return course;
+    }
+    
+    public async Task<Course> CreateCourseAsync(Course course)
+    {
+        _context.Courses.Add(course);
+        await _context.SaveChangesAsync();
+        return course;
+    }
+    
+    public async Task<Course> UpdateCourseAsync(Course course)
+    {
+        _context.Courses.Update(course);
+        await _context.SaveChangesAsync();
+        return course;
+    }
+    
+    public async Task<bool> SoftDeleteCourseByIdAsync(int id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+
+        if (course !=  null)
+        {
+            course.Status = CourseStatus.Deleted;
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> ArchiveCourseByIdAsync(int id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+
+        if (course != null)
+        {
+            course.Status = CourseStatus.Archived;
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+
+        return false;
+    }
+
+    public async Task<Course?> GetCourseByCodeAsync(string code)
+    {
+        return await _context.Courses
+            .FirstOrDefaultAsync(c => c.Code == code && c.Status == CourseStatus.Active);
+    }
+    
+   
+}
---
+++ Homework-track-API/Repositories/CourseRepository/ICourseRepository.cs
@@ -0,0 +1,15 @@
+using Homework_track_API.Entities;
+
+namespace Homework_track_API.Repositories.CourseRepository;
+
+public interface ICourseRepository
+{
+    Task<List<Course>> GetAllCoursesAsync();
+    Task<Course> GetCourseByIdAsync(int id);
+    Task<Course> CreateCourseAsync(Course course);
+    Task<Course?> GetCourseByCodeAsync(string code);
+    Task<Course> UpdateCourseAsync(Course course);
+    Task<bool> SoftDeleteCourseByIdAsync(int id);
+    Task<bool> ArchiveCourseByIdAsync(int id);
+    
+}
