41b174ec4d0efaffbb8a78651492d7c09d5b08a6
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-20 04:20:28 +0300

    added: CourseService

---
+++ Homework-track-API/Services/CourseService/CourseService.cs
@@ -0,0 +1,178 @@
+using Homework_track_API.Entities;
+using Homework_track_API.Enums;
+using Homework_track_API.Repositories.CourseRepository;
+using ArgumentException = System.ArgumentException;
+
+namespace Homework_track_API.Services.CourseService;
+
+public class CourseService(ICourseRepository courseRepository):ICourseService
+{
+    private readonly ICourseRepository _courseRepository = courseRepository;
+
+    public async Task<List<Course>> GetAllCourses()
+    {
+        return await _courseRepository.GetAllCoursesAsync();
+    }
+
+    public async Task<Course> GetCourseById(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid course ID.");
+        }
+
+        var course = await _courseRepository.GetCourseByIdAsync(id);
+
+        if (course == null)
+        {
+            throw new KeyNotFoundException($"Course with ID {id} not found.");
+        }
+
+        return course;
+    }
+
+    public async Task<Course> CreateCourse(Course course)
+    {
+        if (course == null)
+        {
+            throw new ArgumentNullException(nameof(course));
+        }
+
+        if (course.TeacherId <= 0)
+        {
+            throw new ArgumentException("Teacher could not found.");
+        }
+
+        if (string.IsNullOrEmpty(course.Name) && course.Name.Length < 3)
+        {
+            throw new ArgumentException("Course should be has a name.");
+        }
+
+        course.Code = await GenerateUniqueCourseCodeAsync();
+
+        course.Status = CourseStatus.Active;
+
+        return await _courseRepository.CreateCourseAsync(course);
+        
+    }
+    
+    private async Task<string> GenerateUniqueCourseCodeAsync()
+    {
+        string newCode;
+        bool isUnique = false;
+
+        do
+        {
+            string randomPart = GenerateRandomString(10);
+
+            newCode = randomPart;
+            var existingCourse = await _courseRepository.GetCourseByCodeAsync(newCode);
+            if (existingCourse == null)
+            {
+                isUnique = true;
+            }
+        }
+        while (!isUnique);
+
+        return newCode;
+    }
+
+    private string GenerateRandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public async Task<Course?> GetCourseByCode(string code)
+    {
+        if (string.IsNullOrEmpty(code))
+        {
+            throw new ArgumentException("Course should be has a name.");
+        }
+
+        var course  = await _courseRepository.GetCourseByCodeAsync(code);
+
+        if (course == null)
+        {
+            throw new ArgumentNullException(nameof(course));
+        }
+
+        return course;
+    }
+
+    public async Task<Course> UpdateCourse(int id, Course course)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid course ID.");
+        }
+
+        var existedCourse = await _courseRepository.GetCourseByIdAsync(id);
+
+        if (existedCourse == null)
+        {
+            throw new ArgumentNullException(nameof(course));
+        }
+
+        if (!string.IsNullOrEmpty(course.Name))
+        {
+            existedCourse.Name = course.Name;
+        }
+        
+        existedCourse.ImagePath = course.ImagePath ?? existedCourse.ImagePath;
+        existedCourse.Description = course.Description ?? existedCourse.Description;
+        course.Code = existedCourse.Code;
+        course.Homeworks = existedCourse.Homeworks;
+        course.StudentCourses = existedCourse.StudentCourses;
+        course.Status = existedCourse.Status;
+        course.TeacherId = existedCourse.TeacherId;
+
+        return await _courseRepository.UpdateCourseAsync(course);
+    }
+
+    public async Task<bool> SoftDeleteCourseById(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid course ID.");
+        }
+
+        var existingCourse = await _courseRepository.GetCourseByIdAsync(id);
+
+        if (existingCourse == null)
+        {
+            throw new ArgumentNullException(nameof(existingCourse));
+        }
+
+        if (existingCourse.Status == CourseStatus.Deleted)
+        {
+            throw new ArgumentException("Course is already deleted.");
+        }
+
+        return await _courseRepository.SoftDeleteCourseByIdAsync(id);
+    }
+
+    public async Task<bool> ArchiveCourseById(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Invalid course ID.");
+        }
+        
+        var existingCourse = await _courseRepository.GetCourseByIdAsync(id);
+
+        if (existingCourse == null)
+        {
+            throw new ArgumentNullException(nameof(existingCourse));
+        }
+
+        if (existingCourse.Status == CourseStatus.Archived)
+        {
+            throw new InvalidOperationException("Course is already archived.");
+        }
+
+        return await _courseRepository.ArchiveCourseByIdAsync(id);
+    }
+}
---
+++ Homework-track-API/Services/CourseService/ICourseService.cs
@@ -0,0 +1,14 @@
+using Homework_track_API.Entities;
+
+namespace Homework_track_API.Services.CourseService;
+
+public interface ICourseService
+{
+    Task<List<Course>> GetAllCourses();
+    Task<Course> GetCourseById(int id);
+    Task<Course> CreateCourse(Course course);
+    Task<Course?> GetCourseByCode(string code);
+    Task<Course> UpdateCourse(int id, Course course);
+    Task<bool> SoftDeleteCourseById(int id);
+    Task<bool> ArchiveCourseById(int id);
+}
