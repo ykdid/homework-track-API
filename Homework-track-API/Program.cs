1c47d0b9baff14cbd44dac388e982ed1332f0f46
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-20 16:08:17 +0300

    added: injecting repos and services

--- Homework-track-API/Program.cs
+++ Homework-track-API/Program.cs
@@ -1,8 +1,10 @@
 using Homework_track_API.Data;
+using Homework_track_API.Repositories.CourseRepository;
 using Homework_track_API.Repositories.HomeworkRepository;
 using Homework_track_API.Repositories.StudentRepository;
 using Homework_track_API.Repositories.SubmissionRepository;
 using Homework_track_API.Repositories.TeacherRepository;
+using Homework_track_API.Services.CourseService;
 using Homework_track_API.Services.HomeworkService;
 using Homework_track_API.Services.StudentService;
 using Homework_track_API.Services.SubmissionService;
@@ -25,13 +27,13 @@ builder.Services.AddScoped<IHomeworkRepository, HomeworkRepository>();
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
 builder.Services.AddScoped<ISubmissionRepository, SubmissionRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 
 builder.Services.AddScoped<IHomeworkService, HomeworkService>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<ITeacherService, TeacherService>();
 builder.Services.AddScoped<ISubmissionService, SubmissionService>();
-
-
+builder.Services.AddScoped<ICourseService, CourseService>();
 
 builder.Services.AddCors(options =>
 {
