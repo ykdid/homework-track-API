c6bb6b110629cc6e93c2ff03ba944f92931717f3
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-23 17:52:35 +0300

    added: new repo query

--- Homework-track-API/Repositories/CourseRepository/ICourseRepository.cs
+++ Homework-track-API/Repositories/CourseRepository/ICourseRepository.cs
@@ -5,7 +5,7 @@ namespace Homework_track_API.Repositories.CourseRepository;
 public interface ICourseRepository
 {
     Task<List<Course>> GetAllCoursesAsync();
-    Task<Course> GetCourseByIdAsync(int id); 
+    Task<Course> GetCourseByIdAsync(int id);  
     Task<List<Course>> GetCoursesByTeacherId(int id);
     Task<Course> CreateCourseAsync(Course course);
     Task<Course?> GetCourseByCodeAsync(string code);
