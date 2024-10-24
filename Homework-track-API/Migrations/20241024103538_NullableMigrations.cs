ae59bf0de04d1c7d57e2de2c555b1c6302bc82b5
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-24 16:20:17 +0300

    added: new migration file

---
+++ Homework-track-API/Migrations/20241024103538_NullableMigrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homework_track_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
