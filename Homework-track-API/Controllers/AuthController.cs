3cedf07c55c4f1ca549e835efb8d359f7e4b3ca8
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-26 03:22:24 +0300

    added: auth controller.

---
+++ Homework-track-API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Homework_track_API.Services.AuthService;
+using Homework_track_API.DTOs;
+
+namespace Homework_track_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class AuthController(IAuthService authService): ControllerBase
+    {
+        private readonly IAuthService _authService = authService;
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.Register(request);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+
+            return Ok(result);
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.Login(request);
+            if (!result.IsSuccess)
+            {
+                return Unauthorized(result.ErrorMessage);
+            }
+
+            return Ok(result);
+        }
+    }
+}
+
