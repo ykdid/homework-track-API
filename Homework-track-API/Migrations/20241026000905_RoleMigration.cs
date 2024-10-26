d86333b258ea1595de90c6cec47242fe8bc56509
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-26 03:17:30 +0300

    added: new migration file

---
+++ Homework-track-API/Migrations/20241026000905_RoleMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homework_track_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                schema: "Users",
+                table: "Teacher",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                schema: "Users",
+                table: "Student",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "Users",
+                table: "Teacher");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "Users",
+                table: "Student");
+        }
+    }
+}
