b3a32384ee861fe160c0226a25a9cd35e5b0cdc8
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-10-26 03:21:23 +0300

    added: jwt , swagger settings and DI.

--- Homework-track-API/Program.cs
+++ Homework-track-API/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Homework_track_API.Data;
 using Homework_track_API.Repositories.CourseRepository;
 using Homework_track_API.Repositories.HomeworkRepository;
@@ -5,7 +6,9 @@ using Homework_track_API.Repositories.StudentCourseRepository;
 using Homework_track_API.Repositories.StudentRepository;
 using Homework_track_API.Repositories.SubmissionRepository;
 using Homework_track_API.Repositories.TeacherRepository;
+using Homework_track_API.Services.AuthService;
 using Homework_track_API.Services.CourseService;
+using Homework_track_API.Services.EncryptionService;
 using Homework_track_API.Services.HomeworkService;
 using Homework_track_API.Services.StudentCourseService;
 using Homework_track_API.Services.StudentService;
@@ -15,6 +18,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,6 +43,35 @@ builder.Services.AddScoped<ITeacherService, TeacherService>();
 builder.Services.AddScoped<ISubmissionService, SubmissionService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddScoped<IStudentCourseService, StudentCourseService>();
+builder.Services.AddScoped<IEncryptionService, EncryptionService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })  
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.IncludeErrorDetails = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ClockSkew = TimeSpan.Zero,
+        };
+        
+    });
 
 builder.Services.AddCors(options =>
 {
@@ -48,12 +82,42 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
-
+    
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
 });
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RecruitmentAPI", Version = "v1" });
+
+        
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by a space and the JWT token."
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -71,6 +135,8 @@ using (var scope = app.Services.CreateScope())
 
 //app.UseHttpsRedirection();
     
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
