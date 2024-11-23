8eb68eb911ad5a79a71b419bc1d68bc6bc03ca19
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-11-23 03:51:30 +0300

    edited: json serializer

--- Homework-track-API/Program.cs
+++ Homework-track-API/Program.cs
@@ -91,9 +91,13 @@ builder.Services.AddCors(options =>
     });
 });
     
-builder.Services.AddControllers().AddJsonOptions(options =>
+/*builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+});*/
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 
 builder.Services.AddSwaggerGen(c =>
